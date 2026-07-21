c032729231cc09873c4c241ed46977b8505f7d40
WIDEROZ <wideroz@wideroz-pc.home>
2026-07-22 01:58:08 +0200

    fist commit

--- install_px4_gz_ros2_for_ubuntu.sh
+++ install_px4_gz_ros2_for_ubuntu.sh
@@ -28,106 +28,16 @@ cd
 sudo apt-get install git python3-pip -y
 
 
-# Vérifie que fzf est installé
-if ! command -v fzf >/dev/null 2>&1; then
-    sudo apt install fzf
-fi
-
 # ---------- Selecteur de branche PX4 ---------- #
 
 # Sélection du dépôt
-repo="git@github.com:PX4/PX4-Autopilot.git"
-
-
+repo="https://github.com/PX4/PX4-Autopilot.git"
 folder="PX4-Autopilot"
 PX4_DIR="$(pwd)/$folder"
 
-# Vérification que le dossier n'existe déja pas
-if [ -d "$folder" ]; then
-    while true; do
-        read -p "Le dossier $PX4_DIR existe déjà. Voulez-vous le supprimer et choisir la version appropriée ? (Y/N) : " answer
-
-        case "${answer,,}" in
-            y)
-                rm -rf "$folder"
-                echo "Dossier supprimé."
-                
-				# Clone
-				git clone "$repo" "$folder"
-
-				cd "$folder"
-
-				echo "Dépôt cloné dans : $(pwd)"
-
-				# Met à jour les branches distantes
-				git fetch --all --prune
-
-				# Sélection d'une branche
-				branch=$(git branch -a --color=never |
-					sed 's/^[* ]*//' |
-					sed 's#remotes/origin/##' |
-					sort -u |
-					fzf --prompt="Choisir une branche > ")
-
-				if [ -z "$branch" ]; then
-					echo "Aucune branche sélectionnée."
-					exit 0
-				fi
-
-				echo "Checkout de la branche : $branch"
-
-				git checkout "$branch"
-
-				echo "Branche active : $(git branch --show-current)"
-
-				# Update git submodules
-				git submodule update --init --recursive
-				
-				break
-                ;;
-            n)
-				cd "$folder"
-				echo "Branche active : $(git branch --show-current)"
-                break
-                ;;
-            *)
-                echo "Veuillez répondre par Y ou N."
-                ;;
-        esac
-    done
-
-else
-	# Clone
-	git clone "$repo" "$folder"
-
-	cd "$folder"
-
-	echo "Dépôt cloné dans : $(pwd)"
-
-	# Met à jour les branches distantes
-	git fetch --all --prune
-
-	# Sélection d'une branche
-	branch=$(git branch -a --color=never |
-		sed 's/^[* ]*//' |
-		sed 's#remotes/origin/##' |
-		sort -u |
-		fzf --prompt="Choisir une branche > ")
-
-	if [ -z "$branch" ]; then
-		echo "Aucune branche sélectionnée."
-		exit 0
-	fi
-
-	echo "Checkout de la branche : $branch"
-
-	git checkout "$branch"
-
-	echo "Branche active : $(git branch --show-current)"
-
-	# Update git submodules
-	git submodule update --init --recursive
-fi
+# git clone
+cd
+git clone -b release/v1.17 "$repo" --recursive
 
 
 # ---------- Fin du Selecteur de branche PX4 ---------- #
