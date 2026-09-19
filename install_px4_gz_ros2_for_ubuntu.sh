a60d82c8ac7eea0d36e1f08e0d3fbd24f8ceaa7b
WIDEROZ <roftelio001@gmail.com>
2026-09-19 10:24:22 +0200

    script modification for docker

--- install_px4_gz_ros2_for_ubuntu.sh
+++ install_px4_gz_ros2_for_ubuntu.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
-set -x
-
-cd 
-
-touch .profile
+PX4_DIR="$SCRIPT_DIR/.."
 
 set +e
 sudo apt-get update -y
@@ -13,6 +8,8 @@ sudo apt-get upgrade -y
 set -e
 
 sudo apt-get install lsb-release wget gnupg -y
+
+# ---------- Gazebo Install ---------- #
 
 sudo apt-get install wget -y
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
@@ -23,123 +20,21 @@ set -e
 
 sudo apt-get install gz-harmonic -y
 
+# ---------- END Gazebo Install ---------- #
 
-cd
-sudo apt-get install git python3-pip -y
+# ---------- PX4 Setup ---------- #
 
-
-# Vérifie que fzf est installé
-if ! command -v fzf >/dev/null 2>&1; then
-    sudo apt install fzf
-fi
-
-# ---------- Selecteur de branche PX4 ---------- #
-
-# Sélection du dépôt
-repo="git@github.com:PX4/PX4-Autopilot.git"
+sudo apt-get install python3-pip -y
 
 
-folder="PX4-Autopilot"
-PX4_DIR="$(pwd)/$folder"
-
-# Vérification que le dossier n'existe déja pas
-if [ -d "$folder" ]; then
-    while true; do
-        read -p "Le dossier $PX4_DIR existe déjà. Voulez-vous le supprimer et choisir la version appropriée ? (Y/N) : " answer
-
-        case "${answer,,}" in
-            y)
-                rm -rf "$folder"
-                echo "Dossier supprimé."
-                
-				# Clone
-				git clone "$repo" "$folder"
-
-				cd "$folder"
-
-				echo "Dépôt cloné dans : $(pwd)"
-
-				# Met à jour les branches distantes
-				git fetch --all --prune
-
-				# Sélection d'une branche
-				branch=$(git branch -a --color=never |
-					sed 's/^[* ]*//' |
-					sed 's#remotes/origin/##' |
-					sort -u |
-					fzf --prompt="Choisir une branche > ")
-
-				if [ -z "$branch" ]; then
-					echo "Aucune branche sélectionnée."
-					exit 0
-				fi
-
-				echo "Checkout de la branche : $branch"
-
-				git checkout "$branch"
-
-				echo "Branche active : $(git branch --show-current)"
-
-				# Update git submodules
-				git submodule update --init --recursive
-				
-				break
-                ;;
-            n)
-				cd "$folder"
-				echo "Branche active : $(git branch --show-current)"
-                break
-                ;;
-            *)
-                echo "Veuillez répondre par Y ou N."
-                ;;
-        esac
-    done
-
-else
-	# Clone
-	git clone "$repo" "$folder"
-
-	cd "$folder"
-
-	echo "Dépôt cloné dans : $(pwd)"
-
-	# Met à jour les branches distantes
-	git fetch --all --prune
-
-	# Sélection d'une branche
-	branch=$(git branch -a --color=never |
-		sed 's/^[* ]*//' |
-		sed 's#remotes/origin/##' |
-		sort -u |
-		fzf --prompt="Choisir une branche > ")
-
-	if [ -z "$branch" ]; then
-		echo "Aucune branche sélectionnée."
-		exit 0
-	fi
-
-	echo "Checkout de la branche : $branch"
-
-	git checkout "$branch"
-
-	echo "Branche active : $(git branch --show-current)"
-
-	# Update git submodules
-	git submodule update --init --recursive
-fi
-
-
-# ---------- Fin du Selecteur de branche PX4 ---------- #
-
-
+cd $PX4_DIR/
 cp -r "$SCRIPT_DIR/PX4-Autopilot_PATCH/Tools" "$PX4_DIR/"
 
 bash $PX4_DIR/Tools/setup/ubuntu.sh --no-sim-tools
 make px4_sitl
 
 
-sudo apt install python3-colcon-clean python3-opencv -y
+# ---------- END PX4 Setup ---------- #
 
 
 sudo apt autoremove -y
